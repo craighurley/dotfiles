355fb989b9053e1db108898ce7c367b20c722df7
Craig Hurley <craig@thehurley.com>
2013-11-02 15:50:13 +1300

    Corrected repo URL in readme.  Added user check before running to bootstrap.sh.

--- README.md
+++ README.md
@@ -6,15 +6,16 @@ These dotfiles are based on [Mathias Bynens' dotfiles](https://github.com/mathia
 
 ## Installation
 
-1. Install xcode from the app store.
+1. Install xcode from the app store.  Open xcode and accept the license agreement.  TODO: find out how to accept the license via command line.
 2. Install code command line tools:
 ```bash
 xcode-select --install
 ```
 
-3. Install homebrew:
+3. Install homebrew and check that it's ready to go:
 ```bash
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+brew doctor
 ```
 
 4. Install git:
@@ -24,7 +25,7 @@ brew install git
 
 5. Clone this repo:
 ```bash
-git clone https://github.com/mathiasbynens/dotfiles.git ~/.dotfiles 
+git clone https://github.com/craighurley/dotfiles.git ~/.dotfiles 
 ```
 
 6. Run the following commands to backup your original dotfiles and symlink to the new dotfiles, then kill your terminal:
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,22 +2,33 @@
 
 # list of files/folders to symlink in homedir
 files=".aliases .bash_profile .bash_prompt .bashrc .curlrc .exports .extra .functions .gitconfig .gitignore_global .hushlogin .inputrc .path .screenrc .wgetrc"
-
 # path to where you cloned the dotfile repo
 dotfiles=~/.dotfiles
-
 # path to where you want to store a backup of your original dotfiles.
 dotfilesbackup=~/.dotfiles_backup
-if [ ! -d $dotfilesbackup ]; then
-	mkdir -p $dotfilesbackup
+
+function doIt() {
+    # make the dofiles backup directory
+    if [ ! -d $dotfilesbackup ]; then
+       mkdir -p $dotfilesbackup
+    fi
+    
+    # move any existing dotfiles in dotfilesbackup directory, then create symlinks
+    for file in ${files[@]}; do
+        if [ -f $file ]; then
+            echo "Moving ~/$file from ~ to $dotfilesbackup"
+            mv -n ~/$file $dotfilesbackup/
+        fi
+        echo "Creating symlink ~/$file to $dotfiles/$file"
+        ln -s $dotfiles/$file ~/$file
+    done
+}
+
+read -p "This script will backup your current dotfiles and symlink to a new set of dotfiles. Are you sure? (y/n) " -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
 fi
 
-# move any existing dotfiles in dotfilesbackup directory, then create symlinks
-for file in ${files[@]}; do
-    if [ -f $file ]; then
-        echo "Moving ~/$file from ~ to $dotfilesbackup"
-        mv -n ~/$file $dotfilesbackup/
-    fi
-    echo "Creating symlink ~/$file to $dotfiles/$file"
-    ln -s $dotfiles/$file ~/$file
-done
+unset doIt
+exit 0
