a67efb0df1cd05701d97dd48f8a0a774bff72fca
Craig Hurley <craig@thehurley.com>
2015-04-21 18:26:38 +1200

    Added firefox prefs. TODO: find default profile dir and symlink to prefs.

---
+++ applications/firefox/user.js
@@ -0,0 +1,7 @@
+user_pref("layout.spellcheckDefault", 2);
+user_pref("spellchecker.dictionary", "en-GB");
+user_pref("plugins.click_to_play", true);
+user_pref("browser.newtab.url", "https://www.google.co.nz");
+user_pref("browser.fixup.alternate.enabled", false);
+user_pref("keyword.enabled", false);
+user_pref("browser.tabs.tabClipWidth", 1);
