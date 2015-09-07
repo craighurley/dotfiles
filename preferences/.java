d5eb8a98fab1efeb7611abb4a404b658a29c916a
Craig Hurley <craig@thehurley.com>
2015-09-07 21:52:29 +1200

    Add a small script to delete the java plugin.

---
+++ preferences/.java
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# java
+###############################################################################
+
+_plugin=/Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin
+
+if [[ -d "$_plugin" ]]; then
+    sudo rm -fr "$_plugin"
+fi
