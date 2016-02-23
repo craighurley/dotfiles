145b8c979cb7800cf3eb6ff2ad30037e4b19c51d
Craig Hurley <craig@thehurley.com>
2016-02-23 20:49:29 +1300

    Tidy up firefox prefs, and added CTRL+TAB through tabs :)

--- applications/firefox/user.js
+++ applications/firefox/user.js
@@ -1,4 +1,4 @@
-user_pref("accessibility.typeaheadfind", 1);
+user_pref("browser.ctrlTab.previews", true);
 user_pref("browser.fixup.alternate.enabled", false);
 user_pref("browser.newtab.url", "https://www.google.co.nz");
 user_pref("browser.pocket.enabled", false);
@@ -6,7 +6,6 @@ user_pref("browser.tabs.tabClipWidth", 1);
 user_pref("datareporting.healthreport.service.enabled", false);
 user_pref("datareporting.healthreport.uploadEnabled", false);
 user_pref("general.useragent.locale", "en-GB");
-user_pref("keyword.enabled", true);
 user_pref("layout.spellcheckDefault", 2);
 user_pref("loop.enabled", false);
 user_pref("plugins.click_to_play", true);
