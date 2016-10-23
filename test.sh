fc51c0c8eab548d76aeeb57a2a93f6176e8f5b39
Craig Hurley <craig@thehurley.com>
2016-10-23 23:14:58 +1300

    Testing - forcing a failed build.

--- test.sh
+++ test.sh
@@ -2,14 +2,17 @@
 
 set -e
 set -o pipefail
+set -x
 
 (
 if [[ -f "$(which shellcheck)" ]] ; then
     # find all executables and run `shellcheck`
     for f in $(find . -type f -not -iwholename '*.git*' | sort -u) ; do
-        if file "$f" | grep --quiet shell ; then
+        if file "$f" | grep -i --quiet shell; then
             shellcheck "$f" && true
         fi
     done
 fi
 )
+
+echo `basename $0` complete.
