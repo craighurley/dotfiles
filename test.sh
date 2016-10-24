8ca56c0798637e7a1d9d0214a4fb8a7ba2f8fba2
Craig Hurley <craig@thehurley.com>
2016-10-24 22:14:40 +1300

    Switch to ruby language for travis build in order to get access to mdl for linting markdown too.

--- test.sh
+++ test.sh
@@ -4,15 +4,20 @@ set -e
 set -o pipefail
 
 [[ -f "$(which mdl)" ]] && mdl=true || mdl=false
-[[ -f "$(which proselint)" ]] && proselint=true || proselint=false
 [[ -f "$(which shellcheck)" ]] && shellcheck=true || shellcheck=false
 
 # lint all markdown files
-if [[ "$mdl" = true || "$proselint" = true ]] ; then
+if [[ "$mdl" = true ]] ; then
+
+    # create a list of rules to ignore
+    cat << EOF > ~/.mdlrc
+rules "~MD013","~MD014","~MD033"
+EOF
+
+    # find and lint markdown files
     for f in $(find . -type f -name "*.md" -not -iwholename '*.git*' | sort -u) ; do
         if file "$f" | grep -i --quiet "text" ; then
             [[ "$mdl" = true ]] && mdl "$f"
-            [[ "$proselint" = true ]] && proselint "$f"
         fi
     done
 fi
