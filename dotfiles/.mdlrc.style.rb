ffa57e4c9eacafb7923d25e2fbb2c3e51eeb9e6c
Craig Hurley <craig@thehurley.com>
2017-04-17 22:01:44 +1200

    Updated markdown linting rules to use format that is a little easier to manage.

---
+++ dotfiles/.mdlrc.style.rb
@@ -0,0 +1,8 @@
+all
+
+exclude_rule 'MD013' # Line length
+exclude_rule 'MD014' # Dollar signs before commands
+exclude_rule 'MD033' # Inline HTML
+exclude_rule 'MD040' # Code blocks should have a language
+
+rule 'MD007', :indent => 4 # Unordered list indentation
