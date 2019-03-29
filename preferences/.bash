91d5398c2b232e347efea7feaa28fa1897aa8b92
Craig Hurley <craig@thehurley.com>
2019-03-29 11:51:15 +0000

    Double quote to prevent globbing and word splitting.

--- preferences/.bash
+++ preferences/.bash
@@ -2,9 +2,9 @@
 
 vSHELL=/usr/local/bin/bash
 
-if [[ -f $vSHELL ]] ; then
-    if ! grep -q $vSHELL /etc/shells ; then
-        echo $vSHELL >> /etc/shells
-        chsh -s $vSHELL $USER
+if [[ -f "$vSHELL" ]] ; then
+    if ! grep -q "$vSHELL" /etc/shells ; then
+        echo "$vSHELL" >> /etc/shells
+        chsh -s $vSHELL "$USER"
     fi
 fi
