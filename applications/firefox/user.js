463b9196591aebafd9b06fff919a8b5f5f2ce0c7
Craig Hurley <craig@thehurley.com>
2019-10-15 22:58:08 +1300

    Add setting to stop FF 69+ ignoring userChrome.css

--- applications/firefox/user.js
+++ applications/firefox/user.js
@@ -20,4 +20,5 @@ user_pref("privacy.userContext.enabled", true);
 user_pref("privacy.userContext.ui.enabled", true);
 user_pref("security.webauth.u2f", true);
 user_pref("spellchecker.dictionary", "en-GB");
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("toolkit.telemetry.enabled", false);
