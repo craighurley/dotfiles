e01096e0d961dbca6c929aab046c5e439b41e666
Craig Hurley <craig@thehurley.com>
2020-04-25 23:26:46 +1200

    Update tests for gh action

--- test.sh
+++ test.sh
@@ -1,29 +1,14 @@
 #!/usr/bin/env bash
 
-set -e
-set -o pipefail
+set -eo pipefail
 IFS=$'\n'
 
-# install additional tools
-[[ ! -f "$(command -v mdl)" ]] && gem install mdl
-[[ ! -f "$(command -v yaml-lint)" ]] && gem install yaml-lint
-
-# create a list of mdl rules to ignore
-if [[ ! -f ~/.mdlrc ]] ; then
-    cat << EOF > ~/.mdlrc
-rules "~MD013","~MD014","~MD033","~MD046"
-EOF
-fi
-
-# find and lint files
 for f in $(find . -type f -not \( -iwholename '*.git*' -o -iwholename '*.tmp*' \) | sort -u) ; do
     if file "$f" | grep -i --quiet '\(bash\|shell\) script' ; then
         shellcheck "$f"
     elif file "$f" | grep -i --quiet 'text' ; then
-        if [[ "$f" = *.md ]]; then
-            mdl "$f"
-        elif [[ "$f" = *.yaml || "$f" = *.yml ]]; then
-            yaml-lint -i -q "$f"
+        if [[ "$f" = *.yaml || "$f" = *.yml ]]; then
+            yamllint "$f"
         fi
     fi
 done
