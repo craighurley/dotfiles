44356cef6b7233be8810cc1beaff43c3bb7cb724
Craig Hurley <craig@thehurley.com>
2023-06-25 19:27:30 +1200

    Disable pocket by default in firefox

--- applications/firefox/user.js
+++ applications/firefox/user.js
@@ -12,7 +12,7 @@ user_pref("datareporting.healthreport.service.enabled", false);
 user_pref("datareporting.healthreport.uploadEnabled", false);
 user_pref("devtools.webconsole.persistlog", true);
 user_pref("dom.event.clipboardevents.enabled", true);
-user_pref("extensions.pocket.enabled", true);
+user_pref("extensions.pocket.enabled", false);
 user_pref("general.useragent.locale", "en-GB");
 user_pref("layout.spellcheckDefault", 2);
 user_pref("loop.enabled", false);
