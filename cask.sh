55838596731da1405c3810574070c94094432e8d
Craig Hurley <craig@thehurley.com>
2023-08-08 17:14:45 +1200

    Tidy cask apps

--- cask.sh
+++ cask.sh
@@ -13,16 +13,12 @@ sudo -v
 brew tap homebrew/cask-fonts
 
 # install applications
-brew install --cask arq
 brew install --cask appcleaner
-brew install --cask dbeaver-community
+brew install --cask arq
 brew install --cask docker
 brew install --cask firefox --language=en-GB
 brew install --cask iterm2
-brew install --cask microsoft-teams
-brew install --cask remarkable
 brew install --cask visual-studio-code
-brew install --cask zoom
 
 # install unsigned applications
 HOMEBREW_CASK_OPTS="" brew install --cask google-chrome
@@ -31,13 +27,17 @@ HOMEBREW_CASK_OPTS="" brew install --cask google-chrome
 # brew install --cask balsamiq-mockups
 # brew install --cask chromedriver
 # brew install --cask dash
+# brew install --cask dbeaver-community
 # brew install --cask dropbox
+# brew install --cask microsoft-teams
 # brew install --cask raspberry-pi-imager
+# brew install --cask remarkable
 # brew install --cask sourcetree
 # brew install --cask stay
 # brew install --cask tableplus
 # brew install --cask tunnelblick
 # brew install --cask wireshark
+# brew install --cask zoom
 
 # install fonts
 brew install --cask font-hasklig
