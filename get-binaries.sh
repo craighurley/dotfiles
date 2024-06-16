c257038cd3cedce2e07315535f1858c2613b8625
Craig Hurley <craig@thehurley.com>
2024-06-16 13:37:42 +1200

    Update sqlite binaries

--- get-binaries.sh
+++ get-binaries.sh
@@ -10,7 +10,7 @@ download_dir=~/Downloads
 
 curl -Lo $download_dir/awsls.tar.gz https://github.com/jckuester/awsls/releases/download/v0.11.0/awsls_0.11.0_darwin_arm64.tar.gz
 curl -Lo $download_dir/awsrm.tar.gz https://github.com/jckuester/awsrm/releases/download/v0.4.0/awsrm_0.4.0_darwin_arm64.tar.gz
-curl -Lo $download_dir/sqlite-tools-osx-x86.zip https://www.sqlite.org/2023/sqlite-tools-osx-x86-3420000.zip
+curl -Lo $download_dir/sqlite-tools-osx-x86.zip https://sqlite.org/2024/sqlite-tools-osx-x64-3460000.zip
 
 # Finished
 echo "$(basename "$0") complete."
