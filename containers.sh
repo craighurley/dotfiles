d0caed0b304998b79e95c9805b814edf4026baa9
Craig Hurley <craig@thehurley.com>
2025-04-24 21:26:30 +1200

    Update containers

--- containers.sh
+++ containers.sh
@@ -7,10 +7,10 @@ echo "$(basename "$0") starting."
 if [[ -f $(command -v docker) ]] ; then
     echo "Pulling docker images"
     # base containers
-    docker pull alpine:3.18
-    docker pull golang:1-alpine3.18
-    docker pull node:lts-alpine3.18
-    docker pull python:3.11-alpine3.18
+    docker pull alpine
+    docker pull golang:1-alpine
+    docker pull node:lts-alpine
+    docker pull python:3.13-alpine
 
     # personal containers
     docker pull craighurley/aws-mfa:latest
@@ -18,6 +18,8 @@ if [[ -f $(command -v docker) ]] ; then
     docker pull craighurley/cfn-nag:latest
     docker pull craighurley/ipcalc:latest
     docker pull craighurley/markdownlint-cli:latest
+    docker pull craighurley/netcat:latest
+    docker pull craighurley/nmap:latest
     docker pull craighurley/org-formation-cli:latest
     docker pull craighurley/sceptre:latest
     docker pull craighurley/shellcheck:latest
