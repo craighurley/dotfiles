c599451378c61b317226f780d01c6ae4093ccfe6
Craig Hurley <craig@thehurley.com>
2026-01-20 15:10:17 +1300

    Add cline

--- node.sh
+++ node.sh
@@ -9,6 +9,7 @@ npm install -g npm
 # Install node packages
 npm install -g @anthropic-ai/claude-code
 npm install -g @google/gemini-cli
+npm install -g cline
 
 # Finished
 echo "$(basename "$0") complete."
