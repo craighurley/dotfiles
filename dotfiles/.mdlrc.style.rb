2cdacb17b27134d3595fec46e517d142ea666778
Craig Hurley <craig@thehurley.com>
2026-02-14 23:35:19 +1300

    Update mdl rules

--- dotfiles/.mdlrc.style.rb
+++ dotfiles/.mdlrc.style.rb
@@ -5,4 +5,4 @@ exclude_rule 'MD014' # Dollar signs before commands
 exclude_rule 'MD033' # Inline HTML
 exclude_rule 'MD040' # Code blocks should have a language
 
-rule 'MD007', :indent => 4 # Unordered list indentation
+rule 'MD007', :indent => 2 # Unordered list indentation
