b1df4ae6a87cddfdc60117ea95a0435eeac3291c
Craig Hurley <craig@thehurley.com>
2026-02-22 16:17:22 +1300

    Remove/comment out items not used

--- node.sh
+++ node.sh
@@ -7,9 +7,8 @@ echo "$(basename "$0") starting."
 npm install -g npm
 
 # Install node packages
-npm install -g --omit=dev @anthropic-ai/claude-code
-npm install -g --omit=dev @google/gemini-cli
-npm install -g --omit=dev cline
+# npm install -g --omit=dev @google/gemini-cli
+# npm install -g --omit=dev cline
 npm install -g --omit=dev markdownlint-cli
 
 # Finished
